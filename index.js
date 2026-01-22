49db00eb9a939b9e185796ccf57f3c7ce2bfd12e
aavash33 <aavash444pu@gmail.com>
2026-01-22 11:09:31 +1000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/* VIEW PROJECT BUTTON */
+const view_project_button =document.querySelector('.btn_view_project');
+
+
+function view_project_button_func(){
+window.location.href='https://github.com/aavash33/Demo_Photo_card';
+}
+
+view_project_button.addEventListener("click", view_project_button_func);
