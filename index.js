46d4d6f37e25858aa316afba265ef90faf9fcc20
aavash33 <aavash444pu@gmail.com>
2026-01-24 00:13:13 +1000

    new

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const view_project_button =document.querySelector('.btn_view_project');
 
 
 function view_project_button_func(){
-window.location.href='https://github.com/aavash33/Demo_Photo_card';
+window.location.href='https://github.com/aavash33/';
 }
 
 view_project_button.addEventListener("click", view_project_button_func);
