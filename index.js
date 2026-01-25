328ecd832c2de9be40ea345228a3891100954f30
aavash33 <aavash444pu@gmail.com>
2026-01-25 15:57:47 +1000

    new hamenu bar

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ window.location.href='https://github.com/aavash33/';
 }
 
 view_project_button.addEventListener("click", view_project_button_func);
+
+
+/* STYLING HAM BAR */
+
+const hamMenu = document.querySelector('.ham-menu');
+const offScreenMenus = document.querySelector('.off-screen-menus');
+
+hamMenu.addEventListener('click', ()=>{
+    hamMenu.classList.toggle('active');
+    offScreenMenus.classList.toggle('active');
+
+
+    //Change of hamMenu position based on active status
+
+     if(hamMenu.classList.contains('active')) {
+        hamMenu.style.position = 'fixed';
+    } else {
+        hamMenu.style.position = 'absolute';
+    }
+});
+
